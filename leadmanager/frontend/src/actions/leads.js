4cfd88a51e7bc70ec376a48f77e49e0b15473a0c
Brian Meehan <meehanb1@wit.edu>
2020-07-15 20:35:57 -0400

    add lead form

--- leadmanager/frontend/src/actions/leads.js
+++ leadmanager/frontend/src/actions/leads.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-import { GET_LEADS, DELETE_LEAD } from "./types";
+import { GET_LEADS, DELETE_LEAD, ADD_LEAD } from "./types";
 
 // GET_LEADS
 export const getLeads = () => (dispatch) => {
@@ -23,6 +23,19 @@ export const deleteLead = (id) => (dispatch) => {
       dispatch({
         type: DELETE_LEAD,
         payload: id,
+      });
+    })
+    .catch((err) => console.log(err));
+};
+
+// ADD_LEAD
+export const addLead = (lead) => (dispatch) => {
+  axios
+    .post("/api/leads/", lead)
+    .then((response) => {
+      dispatch({
+        type: ADD_LEAD,
+        payload: response.data,
       });
     })
     .catch((err) => console.log(err));
